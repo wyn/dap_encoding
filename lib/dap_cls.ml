70469e2552c52d06e8e383ad0a436bedc0334945
Simon Parry <simon.parry@karoshibee.com>
2022-07-19 16:27:43 +0100

    35 message types half done

--- lib/dap_cls.ml
+++ lib/dap_cls.ml
@@ -263,16 +263,21 @@ module ThreadEvent = struct
 
 end
 
-module Checksum = struct
+module ChecksumAlgorithm = struct
 
-  type checksum_algorithm =
+  type t =
     | MD5
     | SHA1
     | SHA256
     | Timestamp
 
+
+end
+
+module Checksum = struct
+
   type t = {
-    algorithm: checksum_algorithm;
+    algorithm: ChecksumAlgorithm.t;
     checksum: string;
   }
 
@@ -374,7 +379,28 @@ module BreakpointEvent = struct
 
 end
 
-module Module_ = struct type t = (* TODO *) int end
+module Module_ = struct
+
+  type id =
+    | I of int
+    | S of string
+
+  type t = {
+    id: id;
+    name: string;
+    path: string option;
+    isOptimized: bool option;
+    isUserCode: bool option;
+    version: string option;
+    symbolStatus: string option;
+    symbolFilePath: string option;
+    dateTimeStamp: string option;
+    addressRange: string option;
+  }
+
+end
+
+
 module ModuleEvent = struct
 
   type reason =
@@ -440,7 +466,84 @@ module ProcessEvent = struct
 
 end
 
-module Capabilities = struct type t = (* TODO *) int end
+module ExceptionBreakpointsFilter = struct
+
+  type t = {
+    filter: string;
+    label: string;
+    description: string option;
+    default: bool option;
+    supportsCondition: bool option;
+    conditionDescription: string option;
+  }
+
+end
+
+module ColumnDescriptor = struct
+
+  type column_type =
+    | String
+    | Number
+    | Boolean
+    | UnixTimestampUTC
+
+  type t = {
+    attributeName: string;
+    label: string;
+    format: string option;
+    type_: column_type option;
+    width: int64 option
+  }
+
+end
+
+
+module Capabilities = struct
+
+  type t = {
+        supportsConfigurationDoneRequest: bool option;
+        supportsFunctionBreakpoints: bool option;
+        supportsConditionalBreakpoints: bool option;
+        supportsHitConditionalBreakpoints: bool option;
+        supportsEvaluateForHovers: bool option;
+        exceptionBreakpointFilters: ExceptionBreakpointsFilter.t list option;
+        supportsStepBack: bool option;
+        supportsSetVariable: bool option;
+        supportsRestartFrame: bool option;
+        supportsGotoTargetsRequest: bool option;
+        supportsStepInTargetsRequest: bool option;
+        supportsCompletionsRequest: bool option;
+        completionTriggerCharacters: string list option;
+        supportsModulesRequest: bool option;
+        additionalModuleColumns: ColumnDescriptor.t option;
+        supportedChecksumAlgorithms: ChecksumAlgorithm.t list option;
+        supportsRestartRequest: bool option;
+        supportsExceptionOptions: bool option;
+        supportsValueFormattingOptions: bool option;
+        supportsExceptionInfoRequest: bool option;
+        supportTerminateDebuggee: bool option;
+        supportSuspendDebuggee: bool option;
+        supportsDelayedStackTraceLoading: bool option;
+        supportsLoadedSourcesRequest: bool option;
+        supportsLogPoints: bool option;
+        supportsTerminateThreadsRequest: bool option;
+        supportsSetExpression: bool option;
+        supportsTerminateRequest: bool option;
+        supportsDataBreakpoints: bool option;
+        supportsReadMemoryRequest: bool option;
+        supportsWriteMemoryRequest: bool option;
+        supportsDisassembleRequest: bool option;
+        supportsCancelRequest: bool option;
+        supportsBreakpointLocationsRequest: bool option;
+        supportsClipboardContext: bool option;
+        supportsSteppingGranularity: bool option;
+        supportsInstructionBreakpoints: bool option;
+        supportsExceptionFilterOptions: bool option;
+        supportsSingleThreadExecutionRequests: bool option;
+}
+
+end
+
 module CapabilitiesEvent = struct
 
   type body = {
