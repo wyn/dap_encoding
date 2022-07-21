b11b103d862d5ce1bae01d3a33940db1a58dbecc
Simon Parry <simon.parry@karoshibee.com>
2022-07-21 17:17:43 +0100

    bugfixes and printing
    bugfix: only adding path to tbl value list if not already present,
    bugfix: adding dep to tbl when name already present

--- lib/dap.ml
+++ lib/dap.ml
@@ -4,14 +4,15 @@ module Q = Json_query
 
 
 module StrHashtbl = Hashtbl.Make(struct type t = string let equal = String.equal let hash = Hashtbl.hash end)
-let tbl : string list StrHashtbl.t = StrHashtbl.create 100
 
-(* extract all $ref *)
+(* TODO thread these through as module state *)
+let tbl : string list StrHashtbl.t = StrHashtbl.create 100
 let _spaces = ref 0
 
 let space n =
   List.init n (fun _ -> "") |> String.concat " "
 
+(* extract all $ref *)
 let rec process_name ~schema name =
   Printf.printf "%sprocess name start: '%s'\n" (space !_spaces) name;
   _spaces := !_spaces + 4;
@@ -19,7 +20,7 @@ let rec process_name ~schema name =
   (* first check is valid name *)
   let _ = Q.path_of_json_pointer name in
   let element = find_definition name schema in
-  StrHashtbl.add tbl name [];
+  if not @@ StrHashtbl.mem tbl name then StrHashtbl.add tbl name [];
   process_element ~schema ~name element;
 
   _spaces := !_spaces - 4;
@@ -59,13 +60,17 @@ and process_kind ~schema ~name = function
     )
   | Def_ref path ->
     let path_str = Q.json_pointer_of_path path in
-    if StrHashtbl.mem tbl path_str then
-      Printf.printf "%sfound old $ref '%s' under '%s', ignoring\n" (space !_spaces) path_str name
-    else (
-      let ps = StrHashtbl.find_opt tbl name |> Option.value ~default:[] in
+    (* add path_str to the entries under name - but only if not already there *)
+    let ps = StrHashtbl.find_opt tbl name |> Option.value ~default:[] in
+    if not @@ List.mem path_str ps then
       StrHashtbl.replace tbl name (path_str :: ps);
-      Printf.printf "%sfound new $ref '%s' under '%s'\n" (space !_spaces) path_str name;
+    (* if path_str is also a new ref then recurse into it *)
+    if not @@ StrHashtbl.mem tbl path_str then (
+      Printf.printf "%sfound new $ref '%s' under '%s', recursing\n" (space !_spaces) path_str name;
       process_name ~schema path_str
+    )
+    else (
+      Printf.printf "%sfound old $ref '%s' under '%s', not recursing\n" (space !_spaces) path_str name;
     )
   | Id_ref _ -> () (* failwith "TODO Id_ref" *)
   | Ext_ref _ -> () (* failwith "TODO Ext_ref" *)
@@ -82,245 +87,22 @@ and process_property ~schema ~name pname element _required _extra =
   process_element ~schema ~name element
 
 
-let process ~schema name =
-  Printf.printf "\n\nprocessing '%s'\n" name;
-  process_name ~schema name;
-  let ls = StrHashtbl.to_seq tbl |> List.of_seq in
-  let ret = (name, ls) in
-  ret
+let names = function
+| `O fields -> fields |> List.map (fun (nm, _) -> nm)
+| _ -> []
 
 
+let process schema =
+  StrHashtbl.reset tbl;
+  let ns = Q.query [`Field "definitions"] (to_json schema) |> names in
+  Printf.printf "\n\nprocessing '%d' names\n" @@ List.length ns;
+  ns |> List.iter (fun nm -> let name = Printf.sprintf "/definitions/%s" nm in process_name ~schema name);
+  tbl
 
-(* module ProtocolMessage = struct
- *
- *   type msg =
- *     | Request
- *     | Response
- *     | Event
- *
- *   type t = {
- *     seq: int64;
- *     type_: msg;
- *   }
- *
- *   let msg_e =
- *     conv
- *       ( function | Request -> "request" | Response -> "response" | Event -> "event" )
- *       ( fun msg -> match msg with | "request" -> Request | "response" -> Response | "event" -> Event | _ -> failwith "ERROR: unkown msg type" )
- *       string
- *
- *   let e =
- *     let description = "Sequence number (also known as message ID). For protocol messages of type 'request' this ID can be used to cancel the request." in
- *     conv
- *       (fun {seq; type_} -> (seq, type_))
- *       (fun (seq, type_) -> {seq; type_})
- *       (obj2
- *          (req ~description "seq" int64)
- *          (req ~description:"Message type." "type" msg_e))
- * end
- *
- * module Request = struct
- *
- *   type 'args t = {
- *     seq: int64;
- *     type_: ProtocolMessage.msg;
- *     command: string;
- *     arguments: 'args option;
- *   }
- *
- *   let msg_e =
- *     let open ProtocolMessage in
- *     conv
- *       ( function | Request -> "request" | _ -> failwith "ERROR: expected Request")
- *       ( fun msg -> match msg with | "request" -> Request | _ -> failwith "ERROR: expected 'request'" )
- *       string
- *
- *   let e args =
- *     conv
- *       (fun {seq; type_; command; arguments} -> (seq, type_, command, arguments))
- *       (fun (seq, type_, command, arguments) -> {seq; type_; command; arguments})
- *       (obj4
- *          (req "seq" int64)
- *          (req "type" msg_e)
- *          (req "command" string)
- *          (opt "arguments" args))
- *
- * end
- *
- * module Event = struct
- *
- *   type 'body t = {
- *     seq: int64;
- *     type_: ProtocolMessage.msg;
- *     event: string;
- *     body: 'body option;
- *   }
- *
- *   let msg_e =
- *     let open ProtocolMessage in
- *     conv
- *       ( function | Event -> "event" | _ -> failwith "ERROR: expected Event")
- *       ( fun msg -> match msg with | "event" -> Event | _ -> failwith "ERROR: expected 'event'" )
- *       string
- *
- *   let e body =
- *     conv
- *       (fun {seq; type_; event; body} -> (seq, type_, event, body))
- *       (fun (seq, type_, event, body) -> {seq; type_; event; body})
- *       (obj4
- *          (req "seq" int64)
- *          (req "type" msg_e)
- *          (req "event" string)
- *          (opt "body" body))
- *
- * end
- *
- * module Response = struct
- *
- *   type 'body t = {
- *     seq: int64;
- *     type_: ProtocolMessage.msg;
- *     request_seq: int64;
- *     success: bool;
- *     command: string;
- *     message: string option;
- *     body: 'body option;
- *   }
- *
- *   let msg_e =
- *     let open ProtocolMessage in
- *     conv
- *       ( function | Response -> "response" | _ -> failwith "ERROR: expected Response")
- *       ( fun msg -> match msg with | "response" -> Response | _ -> failwith "ERROR: expected 'response'" )
- *       string
- *
- *   let response_e body =
- *     obj7
- *       (req "seq" int64)
- *       (req "type" msg_e)
- *       (req "request_seq" int64)
- *       (req "success" bool)
- *       (req "command" string)
- *       (opt "message" string)
- *       (opt "body" body)
- *
- *   let e body =
- *     conv
- *       (fun {seq; type_; request_seq; success; command; message; body} -> (seq, type_, request_seq, success, command, message, body))
- *       (fun (seq, type_, request_seq, success, command, message, body) -> {seq; type_; request_seq; success; command; message; body})
- *       (response_e body)
- * end
- *
- * module Message = struct
- *   type t = {
- *     id: int64;
- *     format: string;
- *     variables: (string * string) list option;
- *     sendTelemetry: bool option;
- *     showUser: bool option;
- *     url: string option;
- *     urlLabel: string option;
- *   }
- *
- *   let msg_e =
- *     obj7
- *       (req "id" int64)
- *       (req "format" string)
- *       (opt "variables" (list @@ (tup2 string string)))
- *       (opt "sendTelemetry" bool)
- *       (opt "showUser" bool)
- *       (opt "url" string)
- *       (opt "urlLabel" string)
- *
- *   let e =
- *     conv
- *       (fun {id; format; variables; sendTelemetry; showUser; url; urlLabel} -> (id, format, variables, sendTelemetry, showUser, url, urlLabel))
- *       (fun (id, format, variables, sendTelemetry, showUser, url, urlLabel) -> {id; format; variables; sendTelemetry; showUser; url; urlLabel})
- *       msg_e
- * end
- *
- * module Error = struct
- *     type t = {
- *       error: Message.t option;
- *     }
- *
- *     let e =
- *       conv
- *         (fun {error} -> error)
- *         (fun error -> {error})
- *         (obj1 (opt "error" Message.e))
- *
- *   end
- *
- *
- * module ErrorResponse = struct
- *
- *   type t = {
- *     seq: int64;
- *     type_: ProtocolMessage.msg;
- *     request_seq: int64;
- *     success: bool;
- *     command: string;
- *     message: string option;
- *     body: Error.t;
- *   }
- *
- *   let msg_e =
- *     let open ProtocolMessage in
- *     conv
- *       ( function | Response -> "response" | _ -> failwith "ERROR: expected Response")
- *       ( fun msg -> match msg with | "response" -> Response | _ -> failwith "ERROR: expected 'response'" )
- *       string
- *
- *   let response_e =
- *     obj7
- *       (req "seq" int64)
- *       (req "type" msg_e)
- *       (req "request_seq" int64)
- *       (req "success" bool)
- *       (req "command" string)
- *       (opt "message" string)
- *       (req "body" Error.e)
- *
- *   let e =
- *     conv
- *       (fun {seq; type_; request_seq; success; command; message; body} -> (seq, type_, request_seq, success, command, message, body))
- *       (fun (seq, type_, request_seq, success, command, message, body) -> {seq; type_; request_seq; success; command; message; body})
- *       response_e
- * end
- *
- * module CancelArguments = struct
- *   type t = {
- *     requestId: int64 option;
- *     progressId: string option;
- *   }
- *
- *   let e =
- *     conv
- *       (fun {requestId; progressId} -> (requestId, progressId))
- *       (fun (requestId, progressId) -> {requestId; progressId})
- *       (obj2
- *          (opt "requestId" int64)
- *          (opt "progressId" string))
- *
- *   let e_ =
- *     obj2
- *       (opt "requestId" int64)
- *       (opt "progressId" string)
- * end
- *
- * module CancelRequest = struct
- *
- *   let cmd_e =
- *     obj2
- *       (req "command" (constant "cancel"))
- *       (opt "arguments" CancelArguments.e)
- *
- *   let e =
- *     merge_objs (Request.e CancelArguments.e) cmd_e
- * end
- *
- * module CancelResponse = struct
- *   let e =
- *     Response.e unit
- * end *)
+
+let pp_tbl tbl =
+  StrHashtbl.to_seq tbl
+  |> List.of_seq
+  |> List.map (fun (name, deps) -> Printf.sprintf "%s:\n  [ %s ]" name @@ String.concat "; " deps)
+  |> List.sort String.compare
+  |> List.iter (fun ln -> Printf.printf "%s\n\n" ln)
